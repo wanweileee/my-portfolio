c04e5d7501ed76bcebea5c00b3bb36aadd237ce3
wanweileee <wanweileee@gmail.com>
2025-05-07 01:56:20 +0800

    resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -42,6 +42,14 @@ export const NavBar = () => {
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link 
+                  href="/Resume.pdf" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="navbar-link"
+                >
+                  My Resume 
+                </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
