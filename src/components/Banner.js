9ef888be8b07d0d7438fc32576757ad1077e2918
wanweileee <wanweileee@gmail.com>
2025-05-13 01:53:02 +0800

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,6 @@ export const Banner = () => {
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Wan Wei`}</h1>
                 <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Project Manager" , "AI Engineer" , "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
               </div>}
             </TrackVisibility>
           </Col>
